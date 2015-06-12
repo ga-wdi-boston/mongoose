46616c9b6b642b220c97b9554e957b8303afa780
Charlton Wilbur <cwilbur@chromatico.net>
2015-06-12 17:18:49 -0400

    it works terminal-to-database!

--- spec/contact.spec.js
+++ spec/contact.spec.js
@@ -1,18 +1,20 @@
+var util = require('util');
+var mongoose = require('../lib/mongoose-contacts.js');
 var Contact = require('../lib/contacts.js');
 
 describe('the contacts object', function() {
-  describe('the constructor', function() {
-    it('should return a Contacts object', function() {
-      var c = new Contact({
+  describe('Contact.create', function() {
+
+    it('creates a Contact object with the supplied parameters', function(done) {
+      Contact.create({
         name: {
           first: 'Charlton',
           last: 'Wilbur'
         }
-      });
-
-      expect(c.name).toEqual({
-        first: 'Charlton',
-        last: 'Wilbur'
+      }, function(err, c) {
+        expect(c.name.first).toBe('Charlton');
+        expect(c.name.last).toBe('Wilbur');
+        done();
       });
     });
   });
