f4d034d6bc9f4c66a85d2b9d43223927becdfb64
Ross <rdegnen@gmail.com>
2016-05-03 09:54:08 -0400

    fixed issue #17
    moved solution code for person.js and app-people.js from master to this branch

--- app-people.js
+++ app-people.js
@@ -6,32 +6,68 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/mongoose-crud');
 const db = mongoose.connection;
 
+const Person = require('./models/person.js');
+
 const done = function() {
   db.close();
 };
 
-// CRUD Actions
 const create = function(givenName, surname, dob, gender) {
-  /* Add Code Here */
+  Person.create({
+    'name.given': givenName,
+    'name.surname': surname,
+    dob: dob,
+    gender: gender,
+  }).then(function(person) {
+    console.log(person);
+  }).catch(function(error) {
+    console.error(error);
+  }).then(done);
 };
 
 const index = function() {
-  /* Add Code Here */
+  let search = {};
+  if (arguments[0] && arguments[1]) {
+    let field = arguments[0];
+    let criterion = arguments[1];
+    if (criterion[0] === '/') {
+      let regex = new RegExp(criterion.slice(1, criterion.length - 1));
+      search[field] = regex;
+    } else {
+      search[field] = criterion;
+    }
+  }
+
+  Person.find(search).then(function(people) {
+    people.forEach(function(person) {
+      console.log(person.toJSON());
+    });
+  }).catch(console.error).then(done);
 };
 
-const show = function() {
-  /* Add Code Here */
+const show = function(id) {
+  Person.findById(id).then(function(person) {
+    console.log(person.toObject());
+  }).catch(console.error).then(done);
 };
 
 const update = function(id, field, value) {
-  /* Add Code Here */
+  let modify = {};
+  modify[field] = value;
+  Person.findByIdAndUpdate(id, { $set: modify }, { new: true })
+    .then(function(person) {
+      console.log(person.toJSON());
+    }).catch(console.error)
+    .then(done);
 };
 
 const destroy = function(id) {
-  /* Add Code Here */
+  Person.findById(id).then(function(person) {
+    return person.remove();
+  }).catch(console.error
+  ).then(done);
 };
 
-// UI
 db.once('open', function() {
   let command = process.argv[2];
 
@@ -48,7 +84,7 @@ db.once('open', function() {
       if (true || givenName) {
         create(givenName, surname, dob, gender);
       } else {
-        console.log('usage create <given_name> <surname> <date of birth> [gender]');
+        console.log('usage c <given_name> <surname> <date of birth> [gender]');
         done();
       }
       break;
@@ -56,6 +92,17 @@ db.once('open', function() {
     case `show`:
       id = process.argv[3];
       show(id);
+      break;
+
+    case 'search':
+      field  = process.argv[3];
+      let criterion = process.argv[4];
+      if (!criterion) {
+        console.log('usage: search <field> <criterion>');
+        done();
+      } else {
+        index(field, criterion);
+      }
       break;
 
     case 'update':
--- models/person.js
+++ models/person.js
@@ -1,4 +1,52 @@
 // jshint node: true
 'use strict';
 
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+  dob: {
+    type: Date,
+    required: true,
+    match: /\d{4}-\d{2}-\d{2}/
+  },
+  name: {
+    given: {
+      type: String,
+      required: true
+    },
+    surname: {
+      type: String,
+      required: true
+    }
+  },
+  gender : {
+    type: String,
+    enum: {
+      values: ['f', 'm', 'n', 'o']
+    },
+    default: 'o'
+  }
+}, {
+  timestamps: true,
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true }
+});
+
+personSchema.virtual('age').get(function() {
+  let today = new Date();
+  let thisYear = today.getFullYear();
+  if (!this.dob) {
+    return 0;
+  }
+  if (this.dob.getMonth() > today.getMonth() ||
+    this.dob.getMonth() === today.getMonth() &&
+    this.dob.getDate() >= today.getDate()) {
+    thisYear -= 1;
+  }
+  return thisYear - this.dob.getFullYear();
+});
+
+// model
+const Person = mongoose.model('Person', personSchema);
+
 module.exports = Person;
