f88cf5bffdbb8734af07820812defd22b7acdaf4
J-Weeks <weeksjasons@gmail.com>
2016-08-29 16:52:10 -0400

    Updates repo
    from template

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -1,5 +1,37 @@
-describe('a simple test', function() {
-  it('tests that 2+2 = 4', function() {
-    expect(2 + 2).toBe(4);
+'use strict';
+
+// Allow chai syntax like `expect(foo).to.be.ok;`
+// jshint -W030
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+const example = require('../lib/example');
+
+describe('Sync', function () {
+  it('is true', function () {
+    expect(example.sync(true)).to.be.true;
+  });
+});
+
+describe('Async', function () {
+  it('is true', function (done) {
+    example.async(true, function (error, value) {
+      if (error || value !== true) {
+        error = error || new Error(`value is ${value}`);
+      }
+
+      done(error);
+    });
+  });
+});
+
+describe('Promise', function () {
+  it('is true', function () {
+    expect(example.promise(true)).to.eventually.be.true;
   });
 });
