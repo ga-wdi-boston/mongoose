eac9b33bd061ac8617719d75e365bfd9c7f67f81
NatasaPeic <peic.natasa@gmail.com>
2016-08-30 20:56:21 -0400

    Add lab

--- app-places.js
+++ app-places.js
@@ -12,23 +12,72 @@ const done = function() {
 
 // CRUD Actions
 const create = function(name, latitude, longitude, country) {
-  /* Add Code Here */
+  City.create({
+    name: name,
+    latitude: latitude,
+    longitude: longitude,
+    country: country
+  }).then((city) => {
+    console.log(city.toJSON());
+  }).catch(console.error)
+    .then(done);
 };
 
 const index = function(field, criterion) {
-  /* Add Code Here */
+
+  let search = {};
+
+  if (arguments[0] && arguments[1]){
+    let field = arguments[0];
+    let criterion = arguments[1];
+
+    if (criterion[0] === '/') {
+      let regex = new RegExp(criterion.slice(1, criterion.length-1));
+
+      search[field] = regex;
+    } else {
+      search[field] = criterion;
+    }
+  }
+
+  City.find(search).then(function (city) {
+
+    places.forEach(function (city) {
+      console.log(city.toJSON());
+
+    });
+
+  }).catch(console.error)
+    .then(done);
 };
 
 const show = function() {
-  /* Add Code Here */
+  City.findById(id)
+  .then((city) => {
+    console.log(city.toJSON());
+  }).catch(console.error)
+    .then(done);
 };
 
 const update = function(id, field, value) {
-  /* Add Code Here */
+  City.findById(id)
+    .then((city) => {
+
+     city[field] = value;
+
+     return city.save();
+   }).then(function(city){
+     console.log(city.toJSON());
+   }).catch(console.error)
+     .then(done);
 };
 
 const destroy = function(id) {
-  /* Add Code Here */
+  City.findById(id)
+    .then((city) => {
+    city.remove();
+  }).catch(console.error)
+    .then(done);
 };
 
 // UI
@@ -64,6 +113,19 @@ db.once('open', function() {
       let value = process.argv[5];
       update(id, field, value);
       break;
+
+      case 'search':
+        field = process.argv[3];
+        let criterion = process.arg[4];
+        if (!criterion) {
+          console.log('Do this: search <field> <criterion>');
+          done();
+        } else {
+          index(field, criterion);
+        }
+
+        break;
+
 
     case 'destroy':
       id = process.argv[3];
